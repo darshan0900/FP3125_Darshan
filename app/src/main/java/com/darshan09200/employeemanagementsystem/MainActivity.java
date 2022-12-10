1842c656870e399995856cfd363fd69ac9dd365c
Darshan Jain <c0872398@mylambton.ca>
2022-12-10 00:51:46 -0500

    added landscape support; fragment navigation animation; custom back navigation

--- app/src/main/java/com/darshan09200/employeemanagementsystem/MainActivity.java
+++ app/src/main/java/com/darshan09200/employeemanagementsystem/MainActivity.java
@@ -1,8 +1,11 @@
 package com.darshan09200.employeemanagementsystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.darshan09200.employeemanagementsystem.databinding.ActivityMainBinding;
 
@@ -17,11 +20,64 @@ public class MainActivity extends AppCompatActivity implements OnFabClickListene
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        getSupportFragmentManager().beginTransaction().add(R.id.mainFragment, new HomeFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.primaryFragment, new HomeFragment()).commit();
     }
 
     @Override
     public void onFabClick() {
-        getSupportFragmentManager().beginTransaction().replace(R.id.mainFragment, new RegistrationFragment()).commit();
+        boolean isSplitLayoutActive = getResources().getBoolean(R.bool.isSplitLayoutActive);
+        if (isSplitLayoutActive) {
+            hideBackButton();
+            getSupportFragmentManager().beginTransaction().add(R.id.secondaryFragment, new RegistrationFragment()).commit();
+        } else {
+            showBackButton();
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .setCustomAnimations(
+                            R.anim.from_right,
+                            R.anim.to_left,
+                            R.anim.from_left,
+                            R.anim.to_right
+                    )
+                    .add(R.id.primaryFragment, new RegistrationFragment())
+                    .addToBackStack("registration")
+                    .commit();
+        }
+    }
+
+    private void showBackButton() {
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Registration");
+    }
+
+    private void hideBackButton() {
+        getSupportActionBar().setDisplayShowHomeEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setTitle(R.string.app_name);
+    }
+
+    private void simulateBackPressed() {
+        getSupportFragmentManager().popBackStack();
+        hideBackButton();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            simulateBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.primaryFragment);
+        if (fragment instanceof RegistrationFragment) {
+            simulateBackPressed();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
